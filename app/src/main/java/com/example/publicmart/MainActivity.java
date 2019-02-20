bd2d4ddd1d67290a4c402e7fbcddb2848d9aeed7
essensol <help.essensol@gmail.com>
2019-02-20 14:17:31 +0530

    testing 3

--- app/src/main/java/com/example/publicmart/MainActivity.java
+++ app/src/main/java/com/example/publicmart/MainActivity.java
@@ -14,7 +14,7 @@ import io.fabric.sdk.android.Fabric;
 public class MainActivity extends AppCompatActivity {
 
     TextView reg;
-    TextView log;
+    Button log;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-        log = findViewById(R.id.register);
+        log = findViewById(R.id.login);
         log.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
