bd993e65f7f1efac3c2736e78b1e2db93ed85a00
essensol <help.essensol@gmail.com>
2019-02-21 14:10:56 +0530

    Amal Jo Varghese 21 FEB 19 noon
    some changes

--- app/src/main/java/com/example/publicmart/ProductView.java
+++ app/src/main/java/com/example/publicmart/ProductView.java
@@ -1,13 +1,26 @@
 package com.example.publicmart;
 
+import android.os.Handler;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class ProductView extends BaseActivity {
 
     TextView txtxmpny;
+
+    private static ViewPager mPager;
+    private static int currentPage = 0;
+    private static int NUM_PAGES = 0;
+    private static final Integer[] IMAGES= {R.drawable.ad,R.drawable.cosmetics,R.drawable.ad2};
+    private ArrayList<Integer> ImagesArray = new ArrayList<Integer>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,9 +28,55 @@ public class ProductView extends BaseActivity {
         FrameLayout contentFrameLayout = (FrameLayout) findViewById(R.id.content_frame);
         getLayoutInflater().inflate(R.layout.activity_product_view, contentFrameLayout);
 
-
+        init();
 //        android.support.v7.widget.Toolbar tb=getToolBar();
 //        txtxmpny=(TextView)tb.findViewById(R.id.appname);
 //        txtxmpny.setText("Products");
     }
+
+    private void init() {
+
+
+        for (int i = 0; i < IMAGES.length; i++)
+            ImagesArray.add(IMAGES[i]);
+
+        mPager = (ViewPager) findViewById(R.id.pager);
+
+
+        PagerAdapter adapter = new SlidingImage_Adapter(ProductView.this, ImagesArray);
+        mPager.setAdapter(adapter);
+
+
+       // mPager.setAdapter(new SlidingImage_Adapter(ProductView.this, ImagesArray));
+
+
+        final float density = getResources().getDisplayMetrics().density;
+
+
+        NUM_PAGES = IMAGES.length;
+
+
+        // Auto start of viewpager
+//        final Handler handler = new Handler();
+//        final Runnable Update = new Runnable() {
+//            public void run() {
+//                if (currentPage == NUM_PAGES) {
+//                    currentPage = 0;
+//                }
+//                mPager.setCurrentItem(currentPage++, true);
+//            }
+//        };
+//        Timer swipeTimer = new Timer();
+//        swipeTimer.schedule(new TimerTask() {
+//            @Override
+//            public void run() {
+//                handler.post(Update);
+//            }
+//        }, 3000, 3000);
+
+    }
+
+
+
+
 }
